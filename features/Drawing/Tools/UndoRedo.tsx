4aca036ab52f3543859b35b90d86cd2614dcc9f3
gutchom <yskz.tg@gmail.com>
2022-03-20 09:59:47 +0900

    refactor file structure

---
+++ features/Drawing/Tools/UndoRedo.tsx
@@ -0,0 +1,18 @@
+import { FaRedo, FaUndo } from 'react-icons/fa';
+import { useStroke } from '../hooks/useStroke';
+import styles from './undo-redo.module.css';
+
+export function UndoRedo() {
+  const { undo, redo } = useStroke();
+
+  return (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={undo}>
+        <FaUndo />
+      </button>
+      <button className={styles.button} onClick={redo}>
+        <FaRedo />
+      </button>
+    </div>
+  );
+}
--- features/Drawing/index.tsx
+++ features/Drawing/index.tsx
@@ -1,97 +1,7 @@
 import { useRef, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import {
-  FaBrush,
-  FaCheck,
-  FaEraser,
-  FaPaintBrush,
-  FaPenNib,
-  FaRedo,
-  FaUndo,
-} from 'react-icons/fa';
-import { useDrawing, useStroke } from './hooks';
-import type { StrokeColor, StrokeWidth } from './atoms';
-import { colors, strokeColorState, strokeWidthState, widths } from './atoms';
-import styles from './styles.module.css';
-
-export function Pallet() {
-  const { undo, redo } = useStroke();
-  const [strokeColor, setStrokeColor] = useRecoilState(strokeColorState);
-  const [strokeWidth, setStrokeWidth] = useRecoilState(strokeWidthState);
-
-  return (
-    <div className={styles.tools}>
-      <ul className={styles.pallet}>
-        {colors.map((color) => (
-          <li key={color}>
-            <label
-              className={styles.color}
-              style={{
-                background:
-                  color === 'white' && strokeColor === 'white' ? 'gray' : color,
-                color:
-                  color === 'white' && strokeColor !== 'white'
-                    ? '#664'
-                    : 'white',
-                fontSize: color === 'white' ? '14px' : '10px',
-              }}
-            >
-              {color === 'white' && <FaEraser />}
-              {color !== 'white' && color === strokeColor && <FaCheck />}
-              <input
-                style={{ display: 'none' }}
-                type="radio"
-                value={color}
-                checked={color === strokeColor}
-                onChange={(e) => {
-                  setStrokeColor(e.target.value as StrokeColor);
-                }}
-              />
-            </label>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.buttons}>
-        <div className={styles.history}>
-          <button className={styles['history-button']} onClick={undo}>
-            <FaUndo />
-          </button>
-          <button className={styles['history-button']} onClick={redo}>
-            <FaRedo />
-          </button>
-        </div>
-        <ul className={styles.brushes}>
-          {widths.map((width) => (
-            <li key={width}>
-              <label
-                className={styles.brush}
-                style={{
-                  color: width === strokeWidth ? 'white' : '#664',
-                  background: width === strokeWidth ? 'gray' : 'white',
-                }}
-              >
-                {width === 8 && <FaPenNib />}
-                {width === 24 && <FaPaintBrush />}
-                {width === 48 && (
-                  <FaBrush style={{ transform: 'rotate(225deg)' }} />
-                )}
-                <input
-                  style={{ display: 'none' }}
-                  type="radio"
-                  value={width}
-                  checked={width === strokeWidth}
-                  onChange={(e) => {
-                    setStrokeWidth(+e.target.value as StrokeWidth);
-                  }}
-                />
-              </label>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+import { useDrawing } from './hooks/useDrawing';
+import { Tools } from './Tools';
+import styles from './index.module.css';
 
 export default function Drawing() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -124,7 +34,9 @@ export default function Drawing() {
           setTitle(e.target.value);
         }}
       />
-      <Pallet />
+      <div className={styles.tools}>
+        <Tools />
+      </div>
     </div>
   );
 }
