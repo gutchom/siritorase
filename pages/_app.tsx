a6bad2604b86e80186278484e621ce3c968a0613
gutchom <yskz.tg@gmail.com>
2022-03-21 10:57:03 +0900

    add Header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'reset-css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { Header } from 'features/Layout/Header';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Header />
       <Component {...pageProps} />
     </RecoilRoot>
   );
