3a03179ac39f22a6e58af1310c0ea2811f1f696e
gutchom <yskz.tg@gmail.com>
2022-03-21 16:28:54 +0900

    chore

--- features/Drawing/hooks/useDrawing.ts
+++ features/Drawing/hooks/useDrawing.ts
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { useStroke } from './useStroke';
 import { strokeColorState, strokeTypeState, strokeWidthState } from '../atoms';
-import type { Point } from 'features/Drawing/types';
+import type { Point } from '../types';
 
 export function useDrawing(ref: RefObject<HTMLCanvasElement>): {
   start(x: number, y: number): void;
