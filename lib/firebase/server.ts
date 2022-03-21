7e737eb7151f739e09bffec61beb376f11d5942b
gutchom <yskz.tg@gmail.com>
2022-03-21 11:20:05 +0900

    fix firebase admin config

--- lib/firebase/server.ts
+++ lib/firebase/server.ts
@@ -1,13 +1,15 @@
 import admin from 'firebase-admin';
-import { applicationDefault, initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
 
-const app = admin.apps.length
+admin.apps.length
   ? admin.app()
-  : initializeApp({
-      credential: applicationDefault(),
+  : admin.initializeApp({
+      credential: admin.credential.cert({
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY as string,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+      }),
       databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
 
-export const db = getFirestore(app);
+export const db = admin.firestore();
