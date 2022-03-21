6eeb032684b79dd7eba84a459785dde2f7ee9bbe
gutchom <yskz.tg@gmail.com>
2022-03-21 10:55:49 +0900

    fix firebase utility

--- lib/firebase/server.ts
+++ lib/firebase/server.ts
@@ -1,17 +1,13 @@
 import admin from 'firebase-admin';
-import { applicationDefault, initializeApp } from 'firebase-admin/lib/app';
-import type { Firestore } from 'firebase-admin/lib/firestore';
-import { getFirestore } from 'firebase-admin/lib/firestore';
-import type { Storage } from 'firebase-admin/lib/storage';
-import { getStorage } from 'firebase-admin/lib/storage';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-admin.initializeApp();
+const app = admin.apps.length
+  ? admin.app()
+  : initializeApp({
+      credential: applicationDefault(),
+      databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    });
 
-export const app = initializeApp({
-  credential: applicationDefault(),
-  databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-});
-
-export const getFirebaseDb = (): Firestore => getFirestore(app);
-export const getFirebaseStorage = (): Storage => getStorage(app);
+export const db = getFirestore(app);
