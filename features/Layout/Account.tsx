9b12475f04d0d17c071131d770cc569b8072d27e
gutchom <yskz.tg@gmail.com>
2022-04-07 02:50:19 +0900

    implement login with twitter

---
+++ features/Layout/Account.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { FaQuestion, FaSignOutAlt } from 'react-icons/fa';
+import { getFirebaseAuth } from 'lib/firebase/browser';
+import { useAuth } from 'lib/firebase/useAuth';
+import styles from './Account.module.css';
+
+export default function Account() {
+  const auth = getFirebaseAuth();
+  const { user, login, logout } = useAuth(auth);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return !user ? (
+    <button className={styles.login} onClick={login}>
+      Twitterでログイン
+    </button>
+  ) : (
+    <div className={styles.container}>
+      <button className={styles.account} onClick={() => setIsOpen(!isOpen)}>
+        <img
+          className={styles.icon}
+          src={user.photoURL ?? '/default_icon.jpg'}
+          alt="アカウント"
+        />
+      </button>
+      <ul
+        className={styles.buttons}
+        style={{ display: isOpen ? 'flex' : 'none' }}
+      >
+        <li>
+          <Link href="/about">
+            <a className={styles.about}>
+              <FaQuestion />
+            </a>
+          </Link>
+        </li>
+        <li>
+          <button className={styles.logout} onClick={logout}>
+            <FaSignOutAlt />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- features/Layout/Header.tsx
+++ features/Layout/Header.tsx
@@ -1,9 +1,13 @@
+import Account from './Account';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>しりとらせ</h1>
+      <div className={styles.account}>
+        <Account />
+      </div>
     </header>
   );
 }
