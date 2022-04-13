6620276686c5be714e1f0922fdfc7c019dd505e9
gutchom <yskz.tg@gmail.com>
2022-04-14 05:08:57 +0900

    give up to use firebase emulators...

--- lib/firebase/utils/getMediaURL.ts
+++ lib/firebase/utils/getMediaURL.ts
@@ -1,10 +1,5 @@
 export default function getMediaURL(path: string): string {
-  const host =
-    process.env.NEXT_PUBLIC_ENV === 'development'
-      ? `http://${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_EMULATOR_HOST}`
-      : 'https://firebasestorage.googleapis.com';
-
-  return `${host}/v0/b/${
+  return `https://firebasestorage.googleapis.com/v0/b/${
     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
   }/o/${encodeURIComponent(path)}?alt=media`;
 }
