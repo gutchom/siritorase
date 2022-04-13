c982f4af2391cc1cbd7084b71169115149c43ab0
gutchom <yskz.tg@gmail.com>
2022-04-14 05:43:43 +0900

    add link to header title

--- features/Layout/Header.tsx
+++ features/Layout/Header.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 import Account from './Account';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>しりとらせ</h1>
+      <h1 className={styles.title}>
+        <Link href="/">
+          <a className={styles.link}>しりとらせ</a>
+        </Link>      </h1>
       <div className={styles.account}>
         <Account />
       </div>
