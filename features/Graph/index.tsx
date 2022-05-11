95261a9c5e384551a16b88ec5a2f2ed639ad8b14
gutchom <yskz.tg@gmail.com>
2022-05-11 15:52:51 +0900

    siritori network visualization

---
+++ features/Graph/index.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import { useEffect, useRef } from 'react';
+import { Network } from 'vis-network';
+import { options } from './options';
+import type { PictureNode } from 'features/Drawing/types';
+import getNetworkData from './getNetworkData';
+import styles from './index.module.css';
+
+type EventParam = {
+  nodes: number[];
+};
+
+type Props = {
+  pictures: PictureNode[];
+};
+
+export default function Graph(props: Props) {
+  const { pictures } = props;
+  const router = useRouter();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const data = getNetworkData(pictures);
+      const network = new Network(ref.current, data, options);
+      network.on('doubleClick', (param: EventParam) => {
+        const [id] = param.nodes;
+        router.push(`/${id}`);
+      });
+    }
+  }, [ref]);
+
+  return <div className={styles.vis} ref={ref} />;
+}
---
+++ features/Graph/options.ts
@@ -0,0 +1,22 @@
+export const options = {
+  nodes: {
+    borderWidth: 2,
+    borderWidthSelected: 4,
+    color: {
+      border: '#ea6',
+      background: '#fff',
+      highlight: {
+        border: '#2b6',
+        background: '#fff',
+      },
+    },
+    font: { color: '#555' },
+    shapeProperties: {
+      useBorderWithImage: true,
+    },
+  },
+  edges: {
+    color: '#ea6',
+    width: 2,
+  },
+};
