9ce8ae06d519c9b191f5e94dbaeefded5c24b453
gutchom <yskz.tg@gmail.com>
2022-05-17 16:38:46 +0900

    move files

--- features/Drawing/utils/canvasToBlob.ts
+++
@@ -1,7 +0,0 @@
-export default function canvasToBlob(canvas: HTMLCanvasElement): Promise<Blob> {
-  return new Promise((resolve, reject) => {
-    canvas.toBlob((blob) => {
-      blob === null ? reject('blob is null') : resolve(blob);
-    });
-  });
-}
---
+++ features/Drawing/utils/post.ts
@@ -0,0 +1,61 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  increment,
+  setDoc,
+  updateDoc,
+} from '@firebase/firestore';
+import { ref, uploadBytes } from '@firebase/storage';
+import { db, storage } from 'lib/browser/firebase';
+import type { PictureType } from '../types';
+import createOGP from './createOGP';
+
+export default async function post(
+  title: string,
+  ancestors: PictureType[],
+  picture: HTMLCanvasElement,
+  parentImages: HTMLImageElement[],
+): Promise<string> {
+  const id = await register(title, ancestors);
+  await Promise.all([
+    upload(`picture/${id}.png`, await canvasToBlob(picture)),
+    upload(
+      `ogp/${id}.png`,
+      await canvasToBlob(createOGP(picture, parentImages)),
+    ),
+  ]);
+
+  return id;
+}
+
+async function register(
+  title: string,
+  ancestors: PictureType[],
+): Promise<string> {
+  const [ancestor] = ancestors.slice(-1);
+  const created = new Date();
+  const post = { title, ancestors, created, childrenCount: 0 };
+  const postRef = await addDoc(collection(db, 'pictures'), post);
+  if (ancestor) {
+    const childRef = doc(db, 'pictures', ancestor.id, 'children', postRef.id);
+    await setDoc(childRef, post);
+    await updateDoc(doc(db, 'pictures', ancestor.id), {
+      childrenCount: increment(1),
+    });
+  }
+
+  return postRef.id;
+}
+
+async function upload(path: string, blob: Blob) {
+  await uploadBytes(ref(storage, path), blob);
+}
+
+function canvasToBlob(canvas: HTMLCanvasElement): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      blob === null ? reject('blob is null') : resolve(blob);
+    });
+  });
+}
