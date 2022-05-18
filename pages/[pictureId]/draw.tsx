bab9c2b4dafe4196295fb29f0d31e859c8230b1e
gutchom <yskz.tg@gmail.com>
2022-05-18 13:52:39 +0900

    extract getAncesors & pass history to Tweet component

--- pages/[pictureId]/draw.tsx
+++ pages/[pictureId]/draw.tsx
@@ -1,12 +1,12 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { db } from 'lib/server/firebase';
+import getAncestors from 'lib/server/getAncestors';
 import getMediaURL from 'lib/getMediaURL';
 import useMultipleRef from 'lib/useMultipleRef';
 import Ancestors from 'features/Ancestors';
 import Drawing from 'features/Drawing';
-import type { PictureDoc, PictureType } from 'features/Drawing/types';
+import type { PictureType } from 'features/Drawing/types';
 
 type Props = {
   ogp: string;
@@ -50,8 +50,8 @@ const Draw: NextPage<Props> = (props) => {
         <Drawing
           ancestors={ancestors}
           images={images}
-          onComplete={(id) => {
-            router.push(`/${id}/result`);
+          onComplete={async (id) => {
+            await router.push(`/${id}/result`);
           }}
         />
       </main>
@@ -60,18 +60,6 @@ const Draw: NextPage<Props> = (props) => {
 };
 
 export default Draw;
-
-async function getAncestors(id: string): Promise<Omit<PictureType, 'src'>[]> {
-  const snapshot = await db.collection('pictures').doc(id).get();
-  if (!snapshot.exists) {
-    throw new RangeError('"pictures" does not have an id.');
-  }
-  const { title, ancestors, created } = snapshot.data() as PictureDoc;
-
-  return JSON.parse(
-    JSON.stringify([...ancestors, { id, title, created: created.toDate() }]),
-  );
-}
 
 type Params = {
   pictureId: string;
--- pages/[pictureId]/result.tsx
+++ pages/[pictureId]/result.tsx
@@ -3,23 +3,30 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { doc, updateDoc } from '@firebase/firestore';
 import { db } from 'lib/browser/firebase';
+import getAncestors from 'lib/server/getAncestors';
 import getPictures from 'lib/server/getPictures';
-import type { PictureNode } from 'features/Drawing/types';
+import type { PictureNode, PictureType } from 'features/Drawing/types';
 import Graph from 'features/Graph';
 import Tweet from 'features/Tweet';
 import styles from 'styles/result.module.css';
 
 type Props = {
-  pictures: PictureNode[];
   drew: PictureNode;
+  pictures: PictureNode[];
+  ancestors: PictureType[];
 };
 
 const Result: NextPage<Props> = (props) => {
-  const { pictures, drew } = props;
+  const { drew, pictures, ancestors } = props;
   const { src, title, tweetId, userId } = drew;
   const router = useRouter();
   const { pictureId } = router.query;
   const id = typeof pictureId === 'string' ? pictureId : '';
+  const history = ancestors
+    .slice(-3, -1)
+    .map(({ title }) => title)
+    .concat('？？？')
+    .join('→');
 
   return (
     <>
@@ -34,6 +41,7 @@ const Result: NextPage<Props> = (props) => {
           </div>
           <Tweet
             pictureId={id}
+            history={history}
             tweetId={tweetId}
             tweetUserId={userId}
             onTweet={async (tweetId, tweetUserId) => {
@@ -70,11 +78,12 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async (
   }
   const { pictureId } = params;
   const [hostname] = req.headers.host?.split(':') ?? [];
+  const ancestors = await getAncestors(pictureId);
   const pictures = await getPictures(hostname);
   const drew = pictures.find(({ id }) => id === pictureId);
 
   if (drew) {
-    return { props: JSON.parse(JSON.stringify({ pictures, drew })) };
+    return { props: JSON.parse(JSON.stringify({ pictures, drew, ancestors })) };
   } else {
     return { notFound: true };
   }
