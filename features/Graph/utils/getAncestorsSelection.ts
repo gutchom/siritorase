3c443037e82d75c53a768f8827c08c8fd81c7a42
gutchom <yskz.tg@gmail.com>
2022-07-31 09:21:37 +0900

    update getAncestorsSelection.ts

--- features/Graph/utils/getAncestorsSelection.ts
+++ features/Graph/utils/getAncestorsSelection.ts
@@ -1,16 +1,16 @@
-import type { Edge, IdType, Node } from 'vis-network';
+import type { Edge, IdType } from 'vis-network';
 
 export default function getAncestorsSelection(
   targetNode: IdType,
-  nodes: Node[],
   edges: Edge[],
-) {
+): {
+  nodes: IdType[];
+  edges: IdType[];
+} {
   return (function trace(nodeId: IdType): [node: IdType, edge: IdType][] {
     const edge = edges.find((edge) => edge.to === nodeId);
-    return edge
-      ? edge.from && edge.id
-        ? [[edge.from, edge.id], ...trace(edge.from)]
-        : []
+    return edge && edge.from && edge.id
+      ? [[edge.from, edge.id], ...trace(edge.from)]
       : [];
   })(targetNode).reduce<{
     nodes: IdType[];
