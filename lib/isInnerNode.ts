8b39394d000ec7655ede52cd5883e896613509cf
gutchom <yskz.tg@gmail.com>
2022-07-31 09:03:39 +0900

    remove unnecessary files

--- lib/isInnerNode.ts
+++
@@ -1,6 +0,0 @@
-export default function isInnerNode<E extends HTMLElement>(
-  element: E,
-  target: EventTarget,
-): boolean {
-  return target instanceof Node && element.contains(target);
-}
--- lib/usePropChange.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export default function usePropChange<T>(
-  prop: T,
-  callback: (nextProp: T) => void,
-) {
-  useEffect(() => callback(prop), [prop, callback]);
-}
