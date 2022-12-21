4b9bc605e0f1407e112517b08cb290df2f1b475e
gutchom <yskz.tg@gmail.com>
2022-12-22 05:57:10 +0900

    Revert "fix bug in lib/getMediaURL.ts caused by encoding path"
    This reverts commit e9fbe9855ac8b97d0f3cf43a656eeea84fd355ac.

--- lib/getMediaURL.ts
+++ lib/getMediaURL.ts
@@ -4,5 +4,5 @@ export default function getMediaURL(path: string, hostname: string): string {
       ? `http://${hostname}:9199`
       : 'https://firebasestorage.googleapis.com';
   const bucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
-  return `${host}/v0/b/${bucket}/o/${path}?alt=media`;
+  return `${host}/v0/b/${bucket}/o/${encodeURIComponent(path)}?alt=media`;
 }
