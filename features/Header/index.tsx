d3016b8aea5a41851cd6f7a3f261116dadb2ef8a
gutchom <yskz.tg@gmail.com>
2022-12-22 09:48:48 +0900

    delete link to root in header

--- features/Header/index.tsx
+++ features/Header/index.tsx
@@ -1,13 +1,10 @@
-import Link from 'next/link';
 import Account from './Account';
 import styles from './index.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>
-        <Link href="/">しりとらせ</Link>
-      </h1>
+      <h1 className={styles.title}>しりとらせ</h1>
       <div className={styles.account}>
         <Account />
       </div>
