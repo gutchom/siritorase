0ce04c5cca8ab805e4e6bc6ffed98f4aa30cd263
gutchom <yskz.tg@gmail.com>
2023-07-26 03:12:26 +0900

    new environment init

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { readdirSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
+const entries = resolve(__dirname, 'src/entries');
+const input = readdirSync(entries, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .reduce(
+    (result, dirent) => ({
+      ...result,
+      [dirent.name]: resolve(entries, dirent.name, 'index.html'),
+    }),
+    {},
+  );
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input,
+    },
+  },
+});
---
+++ vitest.setup.ts
@@ -0,0 +1,2 @@
+import '@testing-library/dom';
+import 'react-dom/test-utils';
