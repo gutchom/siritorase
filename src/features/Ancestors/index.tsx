22a60f78dbafc134d070c597a56a0f7251cd3327
gutchom <yskz.tg@gmail.com>
2023-11-05 19:02:52 +0900

    add TODO comment to <Ancestors> component

--- src/features/Ancestors/index.tsx
+++ src/features/Ancestors/index.tsx
@@ -5,6 +5,9 @@ type Props = {
   ancestors: PostItem[];
 };
 
+// TODO: 画像の読み込みを遅延させる
+// TODO: 画像の読み込みに失敗した場合の処理を実装する
+// TODO: 画像の読み込み中にローディングアイコンを表示する
 export default function Ancestors(props: Props) {
   const { ancestors } = props;
 
